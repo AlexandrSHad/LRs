ce659cbfc5c8414de05b8f6173c1e847339aebf2
Alexandr SHad <shadurskij@gmail.com>
2018-02-01 19:09:24 +0200

    added async calculator

---
+++ LR35-CalculatorAsync/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LR35_CalculatorAsync
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            Thread.Sleep(a);
+            return a + b;
+        }
+
+        public static Task<int> AddAsync(int a, int b)
+        {
+            return Task.Run(() => Add(a, b));
+        }
+    }
+}
---
+++ LR35-CalculatorAsync/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR35_CalculatorAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var w = new Stopwatch();
+            w.Start();
+
+            DoWork();
+
+            w.Stop();
+            Console.WriteLine(w.ElapsedMilliseconds);
+
+            w.Reset();
+            w.Start();
+
+            //DoWorkAsync();          // run and do not wait
+            //var t = DoWorkAsync();  // run and wait
+            var t = DoWorkParallel();
+            t.Wait();
+
+            w.Stop();
+            Console.WriteLine(w.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        private static void DoWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int r = Calculator.Add(i * 100, 1000);
+
+                Console.WriteLine($"result: {r}");
+            }
+        }
+
+        private static async Task DoWorkAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int r = await Calculator.AddAsync(i * 100, 1000);
+
+                Console.WriteLine($"result: {r}");
+            }
+        }
+
+        private static async Task DoWorkParallel()
+        {
+            var taskList = new List<Task<int>>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Calculator.AddAsync(i * 100, 1000);
+
+                taskList.Add(task);
+            }
+
+            await Task.WhenAll(taskList.ToArray());
+
+            foreach (var task in taskList)
+            {
+                Console.WriteLine($"result: {task.Result}");
+            }
+        }
+    }
+}
