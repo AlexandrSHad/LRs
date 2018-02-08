e38b1e726c12f3dbd51a9b93b35e9a2108c675fe
Alexandr SHad <shadurskij@gmail.com>
2018-02-08 21:55:07 +0200

    addded encryptor

---
+++ LR37-Cryptography/Encryprtor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR37_Cryptography
+{
+    public class Encryprtor
+    {
+        public static void PersistKeyInCSP(string keyContainerName)
+        {
+            new RSACryptoServiceProvider(new CspParameters() { KeyContainerName = keyContainerName });
+        }
+
+        public static byte[] Encrypt(string text, string keyContainerName, bool padding = false)
+        {
+            var byteConverter = new UnicodeEncoding();
+            byte[] dataToEncrypt = byteConverter.GetBytes(text);
+            byte[] encryptedData;
+
+            using (var rsa = new RSACryptoServiceProvider(new CspParameters() { KeyContainerName = keyContainerName }))
+            {
+                encryptedData = rsa.Encrypt(dataToEncrypt, padding);
+            }
+
+            return encryptedData;
+        }
+
+        public static string Decrypt(byte[] dataToDecrypt, string keyContainerName, bool padding = false)
+        {
+            var byteConverter = new UnicodeEncoding();
+
+            byte[] decryptedData;
+
+            using (var rsa = new RSACryptoServiceProvider(new CspParameters() { KeyContainerName = keyContainerName }))
+            {
+                decryptedData = rsa.Decrypt(dataToDecrypt, padding);
+            }
+
+            return byteConverter.GetString(decryptedData);
+        }
+    }
+}
---
+++ LR37-Cryptography/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR37_Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyContainerName = "TestKey";
+            Encryprtor.PersistKeyInCSP(keyContainerName);
+
+            // if you need you may export keys to xml string
+            // rsa.ToXmlString(true)  - open and private key
+            // rsa.ToXmlString(false) - only open key
+            // or export key data to blob
+            // rsa.ExportCspBlob
+            // rsa.ImportCspBlob
+
+            string text = "KyKy!!!";
+
+            byte[] encriptedData = Encryprtor.Encrypt(text, keyContainerName);
+
+            Console.WriteLine("Encrypted data:");
+            foreach (var b in encriptedData)
+            {
+                Console.Write(b.ToString("x") + " ");
+            }
+
+            Console.WriteLine("\r\n\r\nDecrypted data:");
+
+            string decryptedText = Encryprtor.Decrypt(encriptedData, keyContainerName);
+
+            Console.WriteLine(decryptedText);
+
+            Console.ReadLine();
+        }
+    }
+}
