34441ceb176ba65a7d3dfd810b1e575a1c612d38
Alexandr SHad <shadurskij@gmail.com>
2018-02-13 20:42:06 +0200

    implemented unit test for calculator

--- LR34-CalculatorUnitTest/CalculatorTests.cs
+++ LR34-CalculatorUnitTest/CalculatorTests.cs
@@ -8,7 +8,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LR34_CalculatorUnitTest
 {
     [TestClass]
-    class CalculatorTests
+    public class CalculatorTests
     {
+        [TestMethod]
+        public void Div_ShouldReturnCorrectValue()
+        {
+            double a = 10.0;
+            double b = 2.0;
+            double expectedResult = 5.0;
+
+            double result = Calculator.Div(a, b);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Div_ByZero_ShouldThrowDivideByZeroException()
+        {
+            double a = 10.0;
+            double b = 0.0;
+
+            double result = Calculator.Div(a, b);
+        }
     }
 }
