9d4f816987a51d2d8b7c4d51917eb842e8ed2cf7
Alexandr SHad <shadurskij@gmail.com>
2018-02-22 20:41:46 +0200

    added db initializer

---
+++ LR38-WebAPI/App_Start/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using LR38_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace LR38_WebAPI
+{
+    public class DbInitializer : CreateDatabaseIfNotExists<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            var testProductArray = new Product[3]
+            {
+                new Product {
+                    OrderNumber = "OR123",
+                    ProductName = "Product 1",
+                    UnitPrice = 100.00,
+                    Count = 10
+                },
+                new Product {
+                    OrderNumber = "OR356",
+                    ProductName = "Product 2",
+                    UnitPrice = 548.50,
+                    Count = 1
+                },
+                new Product {
+                    OrderNumber = "OR547",
+                    ProductName = "Product 3",
+                    UnitPrice = 12546.20,
+                    Count = 2
+                },
+            };
+
+            context.Products.AddOrUpdate(p => p.ProductName, testProductArray);
+        }
+    }
+}
--- LR38-WebAPI/Global.asax.cs
+++ LR38-WebAPI/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,6 +14,8 @@ namespace LR38_WebAPI
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
