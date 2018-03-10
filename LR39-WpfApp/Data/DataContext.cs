77de09aa3e2f2e75616d4257d6387bf3ef30df0e
Alexandr SHad <shadurskij@gmail.com>
2018-03-10 19:05:32 +0200

    WPF application

---
+++ LR39-WpfApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using LR39_WpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR39_WpfApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LR39-WpfApp/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LR39_WpfApp.Data;
+using LR39_WpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR39_WpfApp.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public List<Product> ProductList { get; set; } = new List<Product>();
+
+        public ProductsViewModel()
+        {
+            var context = new DataContext();
+            ProductList = context.Products.ToList();
+        }
+    }
+}
