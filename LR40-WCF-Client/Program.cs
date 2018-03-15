83f877a222c44d8c50fc8122756c8e008c80e01b
Alexandr SHad <shadurskij@gmail.com>
2018-03-15 20:27:32 +0200

    WCF server and client implemented

---
+++ LR40-WCF-Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR40_WCF_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting...");
+
+            var client = new MathService.MathClient();
+            var factory = client.ChannelFactory;
+            var channel = factory.CreateChannel();
+
+            Console.WriteLine("Connected.");
+
+            while (true)
+            {
+                Console.WriteLine("Input number for get factorial.");
+                Console.Write("> ");
+
+                var n = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(n))
+                {
+                    break;
+                }
+
+                var result = channel.Factorial(Convert.ToInt32(n));
+
+                Console.WriteLine("Factorial {0} is {1}.", n, result);
+            }
+        }
+    }
+}
---
+++ LR40-WCF-Service/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR40_WCF_Service
+{
+    class Math : IMath
+    {
+        public int Factorial(int num)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
